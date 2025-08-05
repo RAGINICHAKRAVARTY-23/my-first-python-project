5fc41f7066c66a251de8685a0af0635a64776624
Ragini Chakravarty <raginichakravarty2011@gmail.com>
2025-08-05 12:47:48 +0530

    Update mad limbs story

--- mad_limbs.py
+++ mad_limbs.py
@@ -1,1 +1,10 @@
-print("hello")
+adjective1 = input("Enter an adjective:")
+noun = input("Enter a noun:")
+adjective_2= input("Enter an ajdective:")
+verb = input("Enter a verb:")
+adjective2 =input("Enter an adjective:")
+
+print(f"Yesterday i went to a  {adjective1} water park")
+print(f"There I saw {noun} ")
+print(f"{noun} was {adjective2} and {verb}ing")
+print(f"I was {adjective_2}")
