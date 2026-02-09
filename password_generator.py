e3e6bcae45d15779e984cce5944b51b7e0b783c1
Ragini Chakravarty <raginichakravarty2011@gmail.com>
2026-02-09 15:19:48 +0530

    Refactor password generator with improved structure

--- password_generator.py
+++ password_generator.py
@@ -1,29 +1,36 @@
+import random , string
+letters = string.ascii_letters
+numbers = string.digits
+symbols = string.punctuation
 
+print("============================WELCOME TO PYTHON PASSWORD GENERATOR============================================")
+def generate_password():
+  letter_range  = int(input("How many letters would you like in your password?:\n"))
+  numbers_range = int(input("How many numbers would you like in your password?:\n"))
+  symbols_range = int(input("How many symbols would you like in your password?:\n"))
 
-import random
+  password_list = []
+  for char in range (0, letter_range):
+    password_list.append(random.choice(letters))
+  for char in range (0, numbers_range):
+    password_list.append(random.choice(numbers))
+  for char in range (0,symbols_range):
+    password_list.append(random.choice(symbols))
+  
+  random.shuffle(password_list)
+  password = "".join(password_list)
+  print(f"Your password has been generated successfully\n{password}")
+generate_password()
 
-letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-numbers = [0,1,2,3,4,5,6,7,8,9]
-symbols = ['!','@','#','$','%','^','&','*','(',')','-','_','=','+','[',']','{','}','|',';',':',"'",'"',',','.','<','>','?','/']
+should_continue = False
+while not should_continue:
+  user_choice = input("Are you satisfied with your password? (y/n):").lower()
+  if user_choice == "y":
+    print("All thanks to Ragini!")
+    should_continue = True
+  elif user_choice == "n":
+    print(f"Here you go again!!")
+    generate_password()
+  else:
+    print ("Please type 'y' or 'n'")
 
-print("Welcome to the Password generator program by python")
-
-letters_range = int(input("How many letters would you like in your password?:\n"))
-numbers_range = int(input("How many numbers would you like in your password?:\n"))
-symbols_range = int(input("How many symbols would you like in your password?:\n"))
-
-password_List = []
-for chars in range(0,letters_range):
-  password_List.append(random.choice(letters))
-for _ in range(numbers_range):
-    password_List.append(str(random.choice(numbers)))
-for chars in range(0,symbols_range):
-  password_List.append(random.choice(symbols))
-
-random.shuffle(password_List)
-
-password = ""
-for chars in password_List:
-  password+=chars
-
-print(f"Your password is {password}")
