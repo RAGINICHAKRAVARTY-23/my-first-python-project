cbf49f02962c95b70eb72a2a47de4bf0e238fde7
Ragini Chakravarty <raginichakravarty2011@gmail.com>
2026-02-11 19:43:08 +0530

    Refactor hangman game into a function

--- hangman.py
+++ hangman.py
@@ -1,44 +1,59 @@
-import random
+import random 
 from hangman_words import word_list
-from hangman_art import stages , logo
+from hangman_art import logo , stages
 print(logo)
 
-chosen_word = random.choice(word_list)
-placeholder = ""
-for i in range(len(chosen_word)):
-  placeholder+="_"
-print(placeholder)
-game_over = False
-lives = 7
-correct_word = []
-while not game_over:
-  print(f"************{lives}/7 lives left************")
-  user_input = input("Guess one letter :\n").lower()
-
-  if user_input in correct_word:
-    print( f"You have already guessed {user_input} ")
-    continue
-  display = ""
+def hangman():
+  chosen_word = random.choice(word_list)
+  placeholder = ""
   for letter in chosen_word:
-    if letter == user_input:
-      display+=letter
-      correct_word.append(letter)
-    elif letter in correct_word:
-      display+=letter
-    else:
-      display+="_"
-  print(display)
+    placeholder+="_"
+  print(placeholder)
 
-  if user_input not in chosen_word:
-    lives-=1
-    print(f"You guessed {user_input} which is incorrect.You loose a life")
+  lives = 7
+  correct_letter = []
+  game_over = False
+  while not game_over:
+    print(f"Your score is - 7/{lives}")
+    user_guess = input("Guess a letter:\n").lower()
+
+    if user_guess in correct_letter:
+      print(f"You have already guessed {user_guess}")
+      continue
+
+    display = ""
+    for letter in chosen_word:
+      if letter == user_guess:
+        display+=letter
+        correct_letter.append(letter)
+      elif letter in correct_letter:
+        display+=letter
+      else:
+        display+="_"
+    print(display)
+
+    if user_guess not in chosen_word:
+      lives -= 1
+      print("Wrong you loose a live!")
+    print(stages[lives])
     if lives == 0:
-      print(f"YOU LOOSE!\n*****The correct word was {chosen_word}******")
-      game_over = True
+        print("You loose!")
+        print(f"The correct word was {chosen_word}.")
+        game_over = True
 
-  if "_" not in display:
-    print(f"YOU WON!!")
-    game_over = True
-  
-  print(stages[lives])
+    if not "_" in display:
+     print("Yeah! you won.")
+     game_over = True
+hangman()
+continue_game = False
+while not continue_game:
+  choice = input("Would you like to continue with the hangman_game? (y/n):\n").lower()
 
+  if choice == "y":
+    print("Here you go!")
+    hangman()
+  elif choice == "n":
+    print("See you later!")
+    break
+  else:
+    print("Please type (y/n)")
